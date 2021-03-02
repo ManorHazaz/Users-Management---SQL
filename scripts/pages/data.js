1745b4184506d3d36588155980b0428ca61fb803
manor hagage <manorhagage@gmail.com>
2021-03-03 00:18:47 +0200

    Change style

--- scripts/pages/data.js
+++ scripts/pages/data.js
@@ -17,16 +17,16 @@ let fieldsName = ['Name', 'Created-at', 'Email' ];
 let fieldsToPrint = ['username', 'usercreatedat', 'useremail'];
 
 // promise to table
-printPromiseToTable( promise, fieldsName, fieldsToPrint, '.data-container' );
+printPromiseToTable( promise, fieldsName, fieldsToPrint, '.users' );
 
 // reload every 3 sec
 setInterval( () => { 
     promise = establishRequest(action,json);
-    printPromiseToTable( promise, fieldsName, fieldsToPrint, '.data-container' ); 
+    printPromiseToTable( promise, fieldsName, fieldsToPrint, '.users' ); 
 }, 3000 );
 
 // logout user
-_('#logout').addEventListener('click', e =>
+_( '#logout' ).addEventListener('click', e =>
 {
     sessionStorage.clear();
     window.location = './';
